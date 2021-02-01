6831208700b3db53aeb5f2af89eeb7c6b7cf2f16
Brev860 <brevan.daniels@bryanuniversity.edu>
2021-01-31 23:41:41 -0500

    week2

--- week 1/list/list.py
+++ week 1/list/list.py
@@ -1,4 +1,4 @@
-list = [1,62,38,4,45,6,7]
+# a = [1,62,38,4,45,6,7]
 
 # def listValue(a):
 #     newlist = []
@@ -25,7 +25,7 @@ list = [1,62,38,4,45,6,7]
 a = [25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
 
 def sortList(a):
-    for i in a[1:]:
+    for i in a:
         j = a.index(i)
     while j > 0 and a[j-1] > a[j]:
         a[j], a[j-1] = a[j-1], a[j]
