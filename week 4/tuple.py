3dd094c64851a8ffa89a7ed430265aa97310044b
Brev860 <60907093+Brev860@users.noreply.github.com>
2021-02-14 21:00:29 -0500

    Add files via upload

--- week 4/tuple.py
+++ week 4/tuple.py
@@ -7,7 +7,9 @@ str_val = 'PythonIsBest'
 flt_val = 24.56
 
 
-print(h(int_val))
-print(h(str_val))
-print(h(flt_val))
-
+print('The integer hash value is when using custom hash func:' + str(h(int_val)))
+print('The integer hash value is when using custom hash func:' + str(h(str_val)))
+print('The integer hash value is when using custom hash func:' + str(h(flt_val)))
+print ("The integer hash value is : " + str(hash(int_val))) 
+print ("The string hash value is : " + str(hash(str_val))) 
+print ("The float hash value is : " + str(hash(flt_val)))
